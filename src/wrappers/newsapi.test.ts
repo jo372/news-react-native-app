b7fe0e81855129998a94024af8d4ece46e0916b9
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-23 17:09:29 +0100

    updated requestConfig to RequestConfig.

--- src/wrappers/newsapi.test.ts
+++ src/wrappers/newsapi.test.ts
@@ -1,11 +1,10 @@
 import { textSpanIntersection } from "typescript";
-import NewsApi, { requestConfig } from "./newsapi";
+import NewsApi, { RequestConfig } from "./newsapi";
 
 // testing if the apikey is ever missing (undefined) i.e. the user hasn't created a .env file, it should return an error.
-
 describe(`it should return '${NewsApi.Error.API_KEY_MISSING}'`, () => {
     it('when no API_KEY is provided / file hasn\'t been created', async() => {
-        const requestConf : requestConfig = {
+        const requestConf : RequestConfig = {
             q: "technology",
             apiKey : undefined
         };
@@ -14,10 +13,30 @@ describe(`it should return '${NewsApi.Error.API_KEY_MISSING}'`, () => {
     })
 })
 
+describe(`it should return '${NewsApi.Error.INVALID_DATE_FORMAT}'`, () => {
+    it('when an invalid date is provided through the a config property called "to" ', async() => {
+        const requestConf : RequestConfig = {
+            q: "technology",
+            to: new Date("21/07/2021")
+        }
+        const request = NewsApi.makeRequest(requestConf);
+        expect(request).rejects.toBe(NewsApi.Error.INVALID_DATE_FORMAT);
+    });
+});
+
+test('it should accept the date if it\'s valid', () => {
+    const requestConf : RequestConfig = {
+        q: "technology",
+        to: new Date("2021/07/21")
+    }
+    const request = NewsApi.makeRequest(requestConf);
+    expect(request).resolves.toBeCalled();
+})
+
 // it should return out of bounds under these conditions
 describe(`it should return '${NewsApi.Error.PAGE_SIZE_OUT_OF_BOUNDS}'`, () => {
     it('when the number is below 0', async() => {
-        const requestConf : requestConfig = {
+        const requestConf : RequestConfig = {
             q: "technology",
             pageSize: -1
         };
@@ -25,7 +44,7 @@ describe(`it should return '${NewsApi.Error.PAGE_SIZE_OUT_OF_BOUNDS}'`, () => {
         expect(request).rejects.toBe(NewsApi.Error.PAGE_SIZE_OUT_OF_BOUNDS);
     });
     it('when the number is higher than 100', async() => {
-        const requestConf : requestConfig = {
+        const requestConf : RequestConfig = {
             q: "technology",
             pageSize : 101
         };
@@ -33,7 +52,7 @@ describe(`it should return '${NewsApi.Error.PAGE_SIZE_OUT_OF_BOUNDS}'`, () => {
         expect(request).rejects.toBe(NewsApi.Error.PAGE_SIZE_OUT_OF_BOUNDS);
     });
     it(`it should return ${NewsApi.Error.PAGE_SIZE_UNDEFINED} when pageSize is never declared`, async() => {
-        const requestConf : requestConfig = {
+        const requestConf : RequestConfig = {
             q: "technology",
             pageSize : undefined
         };
