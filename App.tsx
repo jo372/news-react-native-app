503b4ffa027286314a566d29252da65e7bda00f7
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-24 12:17:43 +0100

    updated to to use our new api within the useEffect() to display basic information to the screen.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import NewsApi, { Article, ResponseData } from './src/wrappers/newsapi';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [articles, setArticles] = useState<Array<JSX.Element>>(new Array<JSX.Element>());
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    NewsApi.makeRequest({
+      q: "technology",
+      to: "2021-07-23"
+    }).then(response => {
+      response.json().then((data : ResponseData) => {
+          const articles = data.articles.map((data: Article, index: number) =>
+            <View key={index}>
+              <Image style={{
+                height: 250,
+                width: 250
+              }} source={{
+                uri: data.urlToImage
+              }}/>
+              <Text>{data.title}</Text>
+              <Text>{data.content}</Text>
+              <Text>{data.author}</Text>
+            </View>
+          );
+
+          console.log(articles);
+          setArticles(articles);
+      });
+    })
+  }, []);
+
+  return <View>
+    { articles }
+  </View>;
+}
