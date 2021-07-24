42e2bde358cc9f8e9c6728884bc360837f6ad220
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-24 12:14:41 +0100

    updated to include testing to check conditions where a date object is provided or a string thats using a date format.

--- src/wrappers/newsapi.test.ts
+++ src/wrappers/newsapi.test.ts
@@ -10,11 +10,11 @@ describe(`it should return '${NewsApi.Error.API_KEY_MISSING}'`, () => {
         };
         const request = NewsApi.makeRequest(requestConf); 
         expect(request).rejects.toBe(NewsApi.Error.API_KEY_MISSING);
-    })
+    });
 })
 
-describe(`it should return '${NewsApi.Error.INVALID_DATE_FORMAT}'`, () => {
-    it('when an invalid date is provided through the a config property called "to" ', async() => {
+describe(`it should return '${NewsApi.Error.INVALID_DATE_FORMAT}' when an invalid date is provided through the a config property called`, () => {
+    it('"to" using a Date object', async() => {
         const requestConf : RequestConfig = {
             q: "technology",
             to: new Date("21/07/2021")
@@ -22,16 +22,50 @@ describe(`it should return '${NewsApi.Error.INVALID_DATE_FORMAT}'`, () => {
         const request = NewsApi.makeRequest(requestConf);
         expect(request).rejects.toBe(NewsApi.Error.INVALID_DATE_FORMAT);
     });
+    it('"to" using a date string', async() => {
+        const requestConf : RequestConfig = {
+            q: "technology",
+            to:"21/07/2021"
+        }
+        const request = NewsApi.makeRequest(requestConf);
+        expect(request).rejects.toBe(NewsApi.Error.INVALID_DATE_FORMAT);
+    });
+    it('"from" using a Date object', async() => {
+        const requestConfig : RequestConfig = {
+            q: "technology",
+            from: new Date("21/07/2021")
+        }
+        const request = NewsApi.makeRequest(requestConfig);
+        expect(request).rejects.toBe(NewsApi.Error.INVALID_DATE_FORMAT);
+    });
+    it('"from" using a date string', async() => {
+        const requestConfig : RequestConfig = {
+            q: "technology",
+            from: "21/07/2021"
+        }
+        const request = NewsApi.makeRequest(requestConfig);
+        expect(request).rejects.toBe(NewsApi.Error.INVALID_DATE_FORMAT);
+    });
 });
 
-test('it should accept the date if it\'s valid', () => {
-    const requestConf : RequestConfig = {
-        q: "technology",
-        to: new Date("2021/07/21")
-    }
-    const request = NewsApi.makeRequest(requestConf);
-    expect(request).resolves.toBeCalled();
-})
+describe('it should accept the date if it\'s valid', () => {
+    it('using a valid date object', async() => {
+        const requestConf : RequestConfig = {
+            q: "technology",
+            to: new Date("2021/07/21")
+        }
+        const request = NewsApi.makeRequest(requestConf);
+        expect(request).resolves.toBeCalled();
+    });
+    it('using a valid date string', async() => {
+        const requestConf : RequestConfig = {
+            q: "technology",
+            to: "2021/07/21"
+        }
+        const request = NewsApi.makeRequest(requestConf);
+        expect(request).resolves.toBeCalled();
+    });
+});
 
 // it should return out of bounds under these conditions
 describe(`it should return '${NewsApi.Error.PAGE_SIZE_OUT_OF_BOUNDS}'`, () => {
