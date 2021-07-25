4596ff75de544e4f313b3f5b876aad3c6913ebe7
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-25 13:05:45 +0100

    updated to include new styling for the application

--- App.tsx
+++ App.tsx
@@ -2,45 +2,94 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import NewsApi, { Article, ResponseData } from './src/wrappers/newsapi';
+
+
+const articleStyle = StyleSheet.create({
+  article: {
+    width: '98%',
+    alignSelf: 'center',
+    marginBottom: 5,
+    marginTop: 5,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    shadowColor: '#222',
+    shadowOffset : { width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 10
+  },
+  articleWrapper: {
+    width: '95%',
+    alignSelf: 'center',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 21
+  },
+  image: {
+    width: 150,
+    height: 150
+  },
+  content: {
+    flexDirection: 'row'
+  },
+  date: {
+    color: '#a7a7a7',
+    fontSize: 12,
+    fontStyle: 'italic'
+  },
+  author: {
+    fontSize: 12,
+    fontStyle: 'italic'
+  },
+  source: {
+    fontSize: 12
+  }
+});
 
 export default function App() {
   const [articles, setArticles] = useState<Array<JSX.Element>>(new Array<JSX.Element>());
 
   useEffect(() => {
     NewsApi.makeRequest({
-      q: "technology",
-      to: "2021-07-23"
+      q: "technology"
     }).then(response => {
       response.json().then((data : ResponseData) => {
-          const articles = data.articles.map((data: Article, index: number) =>
-            <View key={index} style={{
-              alignItems : 'center'
-            }}>
-              <Image style={{
-                height: 250,
-                width: 250
-              }} source={{
-                uri: data.urlToImage
-              }}/>
-              <View style={{
-                alignItems: 'flex-start'
-              }}>
-                <Text>{data.title}</Text>
-                <Text>{data.content}</Text>
-                <Text>{data.author}</Text>
+          const articles = data.articles.map((article: Article, index: number) => {
+            console.log(article);
+            return <View style={articleStyle.article} key={`Article_${Date.now()}_${index}`}>
+              <View style={articleStyle.articleWrapper}>
+                <Text style={articleStyle.title}>{article.title}</Text>
+                <View style={articleStyle.content}>
+                  <Text>{article.description}</Text>
+                  <Image style={articleStyle.image} source={{
+                    uri: article.urlToImage
+                  }}></Image>
+                </View>
+                <View>
+                  <View>
+                    <Text style={articleStyle.author}>{article.author}</Text>
+                    <Text style={articleStyle.source}>{article.source.name}</Text>
+                    <Text style={articleStyle.date}>{new Date(article.publishedAt).toDateString()}</Text>
+                  </View>
+                </View>
               </View>
             </View>
-          );
-
-          console.log(articles);
+          });
           setArticles(articles);
       });
     })
   }, []);
 
-  return <View>
-    { articles }
+  return <View style={{
+    backgroundColor: '#ececec'
+  }}>
+    <ScrollView>
+      { articles }
+    </ScrollView>
   </View>;
 }
